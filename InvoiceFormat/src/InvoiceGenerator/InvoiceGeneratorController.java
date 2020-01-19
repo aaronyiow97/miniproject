cd51dd513894a5925de0a135e74ac15e609a6c05
aaron <nevaran97@gmail.com>
2020-01-19 14:58:16 +0800

    Source Control

---
+++ InvoiceFormat/src/InvoiceGenerator/InvoiceGeneratorController.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package InvoiceGenerator;
+
+/**
+ *
+ * @author Aaron
+ */
+public class InvoiceGeneratorController {
+    private InvoiceGeneratorView view;
+    private InvoiceGeneratorModel model;
+    
+    public InvoiceGeneratorController(){
+        
+    }
+}
---
+++ InvoiceFormat/src/InvoiceGenerator/InvoiceGeneratorModel.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package InvoiceGenerator;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Aaron
+ */
+public class InvoiceGeneratorModel {
+    public InvoiceGeneratorModel(){
+        
+    }
+    
+    public ArrayList<String> getPrdList(){
+        ArrayList<String> prdList = new ArrayList<>();
+        
+        return null;
+        
+    }
+}
